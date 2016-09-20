d13b0fc67e8e70aed6282831514af511617e4bb5
Bojan Draško <drasko_bojan@ymail.com>
2016-09-20 14:57:45 +0200

    player.js

--- js/player.js
+++ js/player.js
@@ -1,5 +1,10 @@
-function pusti(){
-	alert("Shake it!");
-}
-
-$(".content > ul > li > a").click(pusti());
+$(document).ready(function(){
+	function pusti(){
+		alert("Shake it!");
+		var data;
+		$.post( "player.pl",{filename:"pjesma3_1.mp3"}, function(data){
+  			alert(data);
+		});
+	}
+	$(".content > ul > li > a").click(pusti());
+});
