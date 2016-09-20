33efb889f8ad01e436f4f616d62caa3242c7d6cd
Bojan Draško <drasko_bojan@ymail.com>
2016-09-20 15:23:56 +0200

    print fileName

--- player.pl
+++ player.pl
@@ -1,5 +1,6 @@
 #!/usr/bin/perl
 
+use 5.010;
 use strict;
 use warnings;
 use CGI;
@@ -9,5 +10,7 @@ print("Content-type: text/html\n\n");
 my $q = new CGI;
 
 my $filename = $q->param('fileName');
+
+say $q->param('fileName');
 
 system("sudo mplayer Media/$filename > /dev/null 2>&1 &");
